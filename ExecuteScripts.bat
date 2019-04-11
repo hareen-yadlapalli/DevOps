5629a15f28e560f80d5e4fcc6c2d9df5930c5514
Hareen Yadlapalli <hareen.yadlapalli@gmail.com>
2019-04-11 14:34:09 +1000

    Update ExecuteScripts.bat

--- ExecuteScripts.bat
+++ ExecuteScripts.bat
@@ -3,4 +3,4 @@ set %1
 set %2
 echo %DBUserName%
 echo %DBUserPwd%
-REM sqlcmd -S localhost -U %DBUserName% -P %DBUserPwd% -i BuildDir\SQLs\Implementation\01_Impl_SQL.sql -e
+sqlcmd -S localhost -U %DBUserName% -P %DBUserPwd% -i BuildDir\SQLs\Implementation\01_Impl_SQL.sql -e
