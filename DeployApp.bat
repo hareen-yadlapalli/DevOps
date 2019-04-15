998d6368f676e56e533a2e01551e68cd327fef42
Hareen Yadlapalli <hareen.yadlapalli@gmail.com>
2019-04-15 22:43:03 +1000

    Update DeployApp.bat

--- DeployApp.bat
+++ DeployApp.bat
@@ -1,1 +1,1 @@
-curl -u mgrscript:mgrscript -X PUT -F "file=SampleWebApp.war" http://localhost:6080/manager/text/deploy?path=/SampleWebApp
+C:\curl\bin\curl -u mgrscript:mgrscript -X PUT -F "file=SampleWebApp.war" http://localhost:6080/manager/text/deploy?path=/SampleWebApp
