74a9eaa30b32256115d4468b5ca4d9bf27ed44fd
Hareen Yadlapalli <hareen.yadlapalli@gmail.com>
2019-04-18 11:16:18 +1000

    Update stopServer.bat

--- stopServer.bat
+++ stopServer.bat
@@ -5,6 +5,7 @@ cd "C:\Users\Sowmya\Desktop\Hareen\DevOps\apache-tomcat-9.0.17-windows-x64\bin"
 REM setx JAVA_HOME "C:\Program Files\Java\jdk1.8.0_201"
 echo testing
 set JAVA_HOME="C:\ProgramFiles\Java\jdk1.8.0_201"
+set JRE_HOME="C:\Program Files\Java\jre1.8.0_201"
 echo %JAVA_HOME%
 set PATH="%PATH%;%JAVA_HOME%\bin"
 shutdown.bat
