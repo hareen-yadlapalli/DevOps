cb5c8cd936493c8215d667aa91206416cc95dcfa
Hareen Yadlapalli <hareen.yadlapalli@gmail.com>
2019-07-17 16:33:42 +1000

    Update backupApp.bat

--- backupApp.bat
+++ backupApp.bat
@@ -1,7 +1,12 @@
 @echo off
 setlocal enabledelayedexpansion
 set %1
-REM echo serverURL=%serverURL%
+REM echo serverURL=%serverURL% %1
+pwd
+dir
+cd BuildDir\BuildFiles
+pwd
+dir
 curl\bin\curl -u mgrscript:mgrscript -X PUT -F "file=SampleWebApp.war" %serverURL%/manager/text/deploy?path=/SampleWebApp
 
 
