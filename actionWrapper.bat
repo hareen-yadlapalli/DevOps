8d09bec9f29924c250c402db385a32f5dd4770a9
Hareen Yadlapalli <hareen.yadlapalli@gmail.com>
2019-07-17 18:17:03 +1000

    Update actionWrapper.bat

--- actionWrapper.bat
+++ actionWrapper.bat
@@ -23,22 +23,22 @@ for /f "tokens=1,* delims==" %%i in (config.properties) do (
 		)
 	)
 	if "!action!"=="deployApp" (
-		echo action is !action!, target environment is !targetEnv!, property value is !property:~0,8!
-		if "!property:~0,8!"=="!targetEnv!.url" (
+		echo action is !action!, target environment is !targetEnv!, property value is !property:~0,7!
+		if "!property:~0,7!"=="!targetEnv!.url" (
 			REM echo !property! = !value!
 			CALL deployApp.bat "serverURL=!value!"		
 		)
 	)
 	if "!action!"=="stopServer" (
 		echo action is !action!, target environment is !targetEnv!, property value is !property:~0,19!
-		if "!property:~0,19!"=="!targetEnv!.serverHomePath" (
+		if "!property:~0,18!"=="!targetEnv!.serverHomePath" (
 			REM echo !property! = !value!
 			CALL stopServer.bat "serverHomePath=!value!"		
 		)
 	)
 	if "!action!"=="startServer" (
 		echo action is !action!, target environment is !targetEnv!, property value is !property:~0,19!
-		if "!property:~0,19!"=="!targetEnv!.serverHomePath" (
+		if "!property:~0,18!"=="!targetEnv!.serverHomePath" (
 			REM echo !property! = !value!
 			CALL startServer.bat "serverHomePath=!value!"		
 		)
