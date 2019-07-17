48cb6b64d4c2e40b7e32fb1e776a86706ebcf22e
Hareen Yadlapalli <hareen.yadlapalli@gmail.com>
2019-07-17 18:27:11 +1000

    Update deployApp.bat

--- deployApp.bat
+++ deployApp.bat
@@ -3,9 +3,9 @@ setlocal enabledelayedexpansion
 set %1
 REM echo serverURL=%serverURL% %1
 cd BuildDir/BuildFiles
-echo "These files will be deployed"
+echo "These files will be deployed..."
 dir
-curl\bin\curl -u mgrscript:mgrscript -X PUT -F "file=SampleWebApp.war" %serverURL%/manager/text/deploy?path=/SampleWebApp
+REM curl\bin\curl -u mgrscript:mgrscript -X PUT -F "file=SampleWebApp.war" %serverURL%/manager/text/deploy?path=/SampleWebApp
 
 
 
