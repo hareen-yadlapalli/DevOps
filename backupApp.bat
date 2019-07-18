4a52ce6b89d11274740f4f15c2b9406693ff45af
Hareen Yadlapalli <hareen.yadlapalli@gmail.com>
2019-07-18 11:56:10 +1000

    Update backupApp.bat

--- backupApp.bat
+++ backupApp.bat
@@ -1,10 +1,41 @@
 @echo off
 setlocal enabledelayedexpansion
 set %1
+set %2
+REM parameters buildName and serverHomePath
+
+echo Taking full backup of all apps, apps list below...
+for /f "delims=" %%a in ('wmic OS get localdatetime  ^| find "."') do set datetime=%%a
+set "YYYY=%datetime:~0,4%"
+set "MM=%datetime:~4,2%"
+set "DD=%datetime:~6,2%"
+set "HH=%datetime:~8,2%"
+set "MI=%datetime:~10,2%"
+set "SS=%datetime:~12,2%"
+set "fullStamp=!YYYY!-!MM!-!DD!-!HH!-!MI!-!SS!"
+set "backupRepoDir=!buildName!_!fullstamp!"
+echo !backupRepoDir!
+mkdir !backupRepoDir!
+set "backupRepoDirFP=%cd%\!backupRepoDir!"
+set "buildRepoDirFP=%serverHomePath%\webapps"
+echo !backupRepoDirFP!
+echo !buildRepoDirFP!
+xcopy !buildRepoDirFP! !backupRepoDirFP! /E
+cd !backupRepoDir!
+REM echo "# test" >> README.md
+git init
+git add --all
+REM git add README.md
+git commit -m "commiting the backup for build !buildName!"
+git remote add origin https://github.com/hareen-yadlapalli/!backupRepoDir!.git
+git push -u origin master
+REM @echo off
+REM setlocal enabledelayedexpansion
+REM set %1
 REM echo serverHomePath=%serverHomePath%
-cd %serverHomePath%/webapps
-echo "These files will be backed up..."
-dir
+REM cd %serverHomePath%/webapps
+REM echo "These files will be backed up..."
+REM dir
 REM curl\bin\curl -u mgrscript:mgrscript -X PUT -F "file=SampleWebApp.war" %serverURL%/manager/text/deploy?path=/SampleWebApp
 
 
