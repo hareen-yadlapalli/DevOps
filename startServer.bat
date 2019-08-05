b28fdfde91815b7730da8139500a61055109a073
Hareen Yadlapalli <hareen.yadlapalli@gmail.com>
2019-08-05 15:47:10 +1000

    Update startServer.bat

--- startServer.bat
+++ startServer.bat
@@ -1,11 +1,9 @@
 @echo off
 setlocal enabledelayedexpansion
 set %1
-REM echo serverHomePath=%serverHomePath%
-echo starting the server, server path is %serverHomePath%
-REM cd %serverHomePath%\bin
-psexec \\%server_name% -u %server_login% -p %server_password% "%serverHomePath%\startup.bat"
-REM startup.bat
+net use s: %serverName% /u:%userName% %password%
+sc \\%serverName% stop %serviceName%
+
 
 
 
