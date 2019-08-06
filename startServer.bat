2f8e01a3ab0ee2c239915f7cf17edaeee3f1ed31
Hareen Yadlapalli <hareen.yadlapalli@gmail.com>
2019-08-06 15:00:37 +1000

    Update startServer.bat

--- startServer.bat
+++ startServer.bat
@@ -1,8 +1,14 @@
 @echo off
 setlocal enabledelayedexpansion
 set %1
-net use s: %serverName% /u:%userName% %password%
+set %2
+set %3
+set %4
+set %5
+set %6
+net use s: %serverName% /u:%serverUserName% %serverPassword%
 sc \\%serverName% stop %serviceName%
+net use s: /delete
 
 
 
