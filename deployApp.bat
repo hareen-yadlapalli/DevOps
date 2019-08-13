9444eaa0aee0c2defe8cb9454995eaefb57e1316
Hareen Yadlapalli <hareen.yadlapalli@gmail.com>
2019-08-13 16:15:23 +1000

    Update deployApp.bat

--- deployApp.bat
+++ deployApp.bat
@@ -1,6 +1,11 @@
 @echo off
 setlocal enabledelayedexpansion
 set %1
+set %2
+set %3
+echo serverURL=%serverURL%
+echo serverUserName=%serverUserName%
+echo serverPassword=%serverPassword%
 REM echo serverURL=%serverURL% %1
 cd BuildDir/BuildFiles
 echo "These files will be deployed..."
