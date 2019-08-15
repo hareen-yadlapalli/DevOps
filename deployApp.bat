7f062aaa6bee015ff68935b4fff387ebea05af0f
Hareen Yadlapalli <hareen.yadlapalli@gmail.com>
2019-08-15 22:34:52 +1000

    Update deployApp.bat

--- deployApp.bat
+++ deployApp.bat
@@ -10,7 +10,10 @@ REM echo serverURL=%serverURL% %1
 cd BuildDir/BuildFiles
 echo "These files will be deployed..."
 dir
-FOR %%i IN (*.*) DO echo %%i
+FOR %%i IN (*.war) DO (
+  set fileName=%%i
+  echo !fileName!
+  )
 REM for /f "tokens=*" %%G in ('dir /b /s /a:d "*.war"') do echo Found %%G
 REM curl\bin\curl -u mgrscript:mgrscript -X PUT -F "file=SampleWebApp.war" %serverURL%/manager/text/deploy?path=/SampleWebApp
 
