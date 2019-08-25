4c41086c21156d634b20ce137328baf878a82d57
Hareen Yadlapalli <hareen.yadlapalli@gmail.com>
2019-08-25 11:37:27 +1000

    Update deployApp.bat

--- deployApp.bat
+++ deployApp.bat
@@ -5,7 +5,7 @@ set %2
 set %3
 echo serverURL=%serverURL%
 echo appServerUserName=%appServerUserName%
-echo appServerPassword=%appServerPassword%
+echo appServerPwd=%appServerPwd%
 REM echo serverURL=%serverURL% %1
 cd BuildDir/BuildFiles
 echo "These files will be deployed..."
@@ -15,7 +15,7 @@ FOR %%i IN (*.war) DO (
   echo !fileName!
   set fileNameWOExt=%%~ni
   echo %%~ni
-  ..\..\curl\bin\curl -u %appServerUserName%:%appServerPassword% -X PUT -F "file=!fileName!" %serverURL%/manager/text/deploy?path=/SampleWebApp
+  ..\..\curl\bin\curl -u %appServerUserName%:%appServerPwd% -X PUT -F "file=!fileName!" %serverURL%/manager/text/deploy?path=/SampleWebApp
   )
 REM for /f "tokens=*" %%G in ('dir /b /s /a:d "*.war"') do echo Found %%G
 REM curl\bin\curl -u mgrscript:mgrscript -X PUT -F "file=SampleWebApp.war" %serverURL%/manager/text/deploy?path=/SampleWebApp
